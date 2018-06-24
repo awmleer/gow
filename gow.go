6bb7f3d99e089f8ed6bcd8dec9aa1f9aa3bb7731
awmleer <awmleer@sparker.xyz>
2018-06-24 23:43:26 +0800

    init code

---
+++ gow.go
@@ -0,0 +1,21 @@
+package gow
+
+import "net"
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		// handle error
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	conn.Read()
+}
